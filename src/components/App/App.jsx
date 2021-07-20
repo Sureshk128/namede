5671c0499365f0d462f4b9feb4ed8fe1fd6b57d7
Suresh Kumamr <choudharysureshk128@gmail.com>
2021-07-20 12:34:24 +0530

    first change to github

---
+++ src/components/App/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Header from './../Header/Header'
+import SearchBox from '../SearchBox/SearchBox';
+class App extends React.Component {
+    state={
+        headerText:'Name De!',
+        };
+    render() {
+        return (
+            <div>
+                <Header headTitle={this.state.headerText}/>
+                 <SearchBox />           
+            </div>
+        );
+    }
+}
+export default App;
---
+++ src/components/SearchBox/SearchBox.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './SearchBox.css';
+const SearchBox=()=>{
+return(
+   <div className="search-container">
+       <input type="text" placeholder="Type Keywords" class="search-input"/>
+   </div>
+);
+};
+export default SearchBox;
