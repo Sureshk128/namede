a6424b7c382925f2cc168cd143ef859a872e80db
Suresh Kumamr <choudharysureshk128@gmail.com>
2021-07-22 11:35:46 +0530

    final commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import { classDeclaration } from '@babel/types';
 import React from 'react';
 import './Header.css'
-const Header = (props) => {
+const Header = ({headTitle,headerExpanded}) => {
     return (
         <div className="head-container">
-            <img src="https://user-images.githubusercontent.com/26179770/106359099-693e3380-6336-11eb-8069-9e36f25de5ca.png" className="Header-image" />
-            <h1 className="head-text">{props.headTitle}</h1>
+            <img src="https://user-images.githubusercontent.com/26179770/106359099-693e3380-6336-11eb-8069-9e36f25de5ca.png" 
+            className={`Header-image ${headerExpanded?'Header-image-expanded' : 'Header-image-contracted'}`} />
+            <h1  className={`head-text ${headerExpanded?'head-text-expanded' : 'head-text-contracted'}`}>{headTitle}</h1>
         </div>
     );
 };
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './SearchBox.css';
-const SearchBox=()=>{
+const SearchBox=({onInputChange})=>{
 return(
    <div className="search-container">
-       <input type="text" placeholder="Type Keywords" class="search-input"/>
+       <input 
+       onChange={(event)=>onInputChange(event.target.value)}
+       type="text" placeholder="Type Keywords" className="search-input"/>
    </div>
 );
 };
